8958b8b50081d33f1ddca0925273a076d40eae64
murilogit <murilograssi@yahoo.com.br>
2015-03-14 15:03:25 -0300

    Lista 2 dos exercícios de POO

---
+++ POO-2015/Lista2/src/Triangle.java
@@ -0,0 +1,23 @@
+public class Triangle {
+	double area;
+	int height;
+	int length;
+
+	public static void main(String args[]) {
+		_________
+		_____________________
+		while(_________) {
+			_____________________
+			______.height = (x + 1) * 2;
+			______.length = x + 4;
+			_____________________
+			System.out.print("triangulo " + x + ", area");
+			System.out.println(" = " + ______.area);
+			_____________________
+		}
+	}
+
+	void setArea() {
+		__________ = (height * length) / 2;
+	}
+}
